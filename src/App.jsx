5bf426a9bf770c63530ed88e1208e9825e624f8d
Sajith Bandara <140834328+sajith23420@users.noreply.github.com>
2025-03-05 19:26:32 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+  
+
+  return (
+    <div>
+      <ProductCard
+      name="Mini JBL"
+      
+      price="3250/-"
+
+      img="https://img.drz.lazcdn.com/g/kf/S6cfb0ee8d9f74ebb9660fd04b3622e364.jpg_720x720q80.jpg_.webp"
+
+      description="JBL Charge Mini 3+ Portable Wireless Bluetooth Speaker With FM Radio JBL"/>
+
+      <ProductCard 
+      name ="Audio Interface with DJ Mixer "
+      
+      price = "5630/-"
+      
+      img="https://img.drz.lazcdn.com/g/kf/S4e6b730b8df84d628a7c0c89e1bbe471H.jpg_400x400q75.avif "
+      
+      description ="Live Sound Card and Audio Interface with DJ Mixer Effects and Voice Changer,Prefect for Streaming/Podcasting/Gaming"/>
+                             
+       </div>
+  )
+}
+
+export default App
