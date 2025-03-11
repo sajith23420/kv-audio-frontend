5f725dc402ff03bb7f911cce05680a6c10dc64e3
Sajith Bandara <sajithbandara0804@gmail.com>
2025-03-11 14:08:54 +0530

    install react icons and test it

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css'
-import ProductCard from './components/productCard'
+import './App.css';
+import { MdOutlineBluetoothAudio } from "react-icons/md";
 
 function App() {
-  
-
   return (
-   <div className=''> 
+    <div className="w-full h-screen flex">
+      <div className="w-[400px] h-full bg-green-200">
+      </div>
+      <div className='w-full bg-red-900'>
+      <MdOutlineBluetoothAudio className='text-[300px] text-white'/>
 
+      </div>
+    </div>
 
-
-   </div>
   )
 }
 
